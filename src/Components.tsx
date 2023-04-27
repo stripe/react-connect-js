305d4ffa819f86eea0ed016d42f7968703734a49
Joanna Li <jojo@stripe.com>
2023-04-26 22:24:07 -0700

    onboarding

--- src/Components.tsx
+++ src/Components.tsx
@@ -47,9 +47,11 @@ export const ConnectPaymentDetails = ({
   return wrapper;
 };
 
-export const ConnectAccountOnboarding = (
-  onOnboardingComplete: () => void
-): JSX.Element | null => {
+export const ConnectAccountOnboarding = ({
+  onOnboardingComplete,
+}: {
+  onOnboardingComplete: () => void;
+}): JSX.Element | null => {
   const {wrapper, component: onboarding} = useCreateComponent(
     'stripe-connect-account-onboarding'
   );
