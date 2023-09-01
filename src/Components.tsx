9b11352d5c2644ab58c0d639daed546c615dbd7a
Joanna Li <jojo@stripe.com>
2023-08-31 19:20:30 -0700

    Upgrade package and add onboarding component

--- src/Components.tsx
+++ src/Components.tsx
@@ -3,12 +3,12 @@ import {useAttachAttribute} from './utils/useAttachAttribute';
 import {ConnectElementEventNames, useAttachEvent} from './utils/useAttachEvent';
 
 export const ConnectPayments = (): JSX.Element => {
-  const {wrapper} = useCreateComponent('stripe-connect-payments');
+  const {wrapper} = useCreateComponent('payments');
   return wrapper;
 };
 
 export const ConnectPayouts = (): JSX.Element => {
-  const {wrapper} = useCreateComponent('stripe-connect-payouts');
+  const {wrapper} = useCreateComponent('payouts');
   return wrapper;
 };
 
@@ -21,13 +21,29 @@ export const ConnectPaymentDetails = ({
   onClose: () => void;
   visible?: boolean | undefined;
 }): JSX.Element | null => {
-  const {wrapper, component: paymentDetails} = useCreateComponent(
-    'stripe-connect-payment-details'
-  );
+  const {wrapper, component: paymentDetails} =
+    useCreateComponent('payment-details');
 
   useAttachEvent(paymentDetails, ConnectElementEventNames.close, onClose);
   useAttachAttribute(paymentDetails, 'charge-id', chargeId);
   useAttachAttribute(paymentDetails, 'visible', visible);
+
+  return wrapper;
+};
+
+export const ConnectAccountOnboarding = ({
+  onOnboardingExited,
+}: {
+  onOnboardingExited: () => void;
+}): JSX.Element | null => {
+  const {wrapper, component: onboarding} =
+    useCreateComponent('account-onboarding');
+
+  useAttachEvent(
+    onboarding,
+    ConnectElementEventNames.onboardingExited,
+    onOnboardingExited
+  );
 
   return wrapper;
 };
