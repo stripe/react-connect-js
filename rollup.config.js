a59c0ccdebf92b548bb4dc1d2de83725a39dd0c4
jojo-stripe <124314569+jojo-stripe@users.noreply.github.com>
2023-09-01 15:10:33 -0700

    Jojo/json (#24)
    * v1.3.0
    
    * add json
    
    * package

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,45 @@
-import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
-import replace from "rollup-plugin-replace";
-import ts from "rollup-plugin-ts";
-import pkg from "./package.json";
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import {terser} from 'rollup-plugin-terser';
+import replace from 'rollup-plugin-replace';
+import ts from 'rollup-plugin-ts';
+import pkg from './package.json';
+import json from '@rollup/plugin-json';
 
 const PLUGINS = [
   ts({
-    tsconfigOverride: { exclude: ["**/*.test.ts"] },
+    tsconfigOverride: {exclude: ['**/*.test.ts']},
   }),
   babel({
-    extensions: [".ts", ".js", ".tsx", ".jsx"],
+    extensions: ['.ts', '.js', '.tsx', '.jsx'],
   }),
   replace({
     _VERSION: JSON.stringify(pkg.version),
   }),
+  json(),
 ];
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
-      { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "es" },
+      {file: pkg.main, format: 'cjs'},
+      {file: pkg.module, format: 'es'},
     ],
     plugins: PLUGINS,
   },
   // UMD build with inline PropTypes
   {
-    input: "src/index.ts",
-    external: ["react"],
+    input: 'src/index.ts',
+    external: ['react'],
     output: [
       {
-        name: "ReactConnect",
+        name: 'ReactConnect',
         file: pkg.browser,
-        format: "umd",
+        format: 'umd',
         globals: {
-          react: "React",
+          react: 'React',
         },
       },
     ],
