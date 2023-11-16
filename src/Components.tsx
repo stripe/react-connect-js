6cf4679a40d957905db2c022d976256736a1d4ff
kaiying-stripe <110323300+kaiying-stripe@users.noreply.github.com>
2023-11-16 14:45:06 -0800

    cherry pick use custom setter methods

--- src/Components.tsx
+++ src/Components.tsx
@@ -1,6 +1,6 @@
+import React from 'react';
 import {useCreateComponent} from './useCreateComponent';
 import {useAttachAttribute} from './utils/useAttachAttribute';
-import {ConnectElementEventNames, useAttachEvent} from './utils/useAttachEvent';
 
 export const ConnectAccountOnboarding = ({
   onExit,
@@ -18,26 +18,21 @@ export const ConnectAccountOnboarding = ({
   const {wrapper, component: onboarding} =
     useCreateComponent('account-onboarding');
 
-  useAttachAttribute(
+  React.useEffect(() => {
+    if (!onboarding) return;
+    onboarding.setRecipientTermsOfServiceUrl(recipientTermsOfServiceUrl);
+    onboarding.setFullTermsOfServiceUrl(fullTermsOfServiceUrl);
+    onboarding.setPrivacyPolicyUrl(privacyPolicyUrl);
+    onboarding.setSkipTermsOfServiceCollection(skipTermsOfServiceCollection);
+    onboarding.setOnExit(onExit);
+  }, [
+    fullTermsOfServiceUrl,
+    onExit,
     onboarding,
-    'recipient-terms-of-service-url',
-    recipientTermsOfServiceUrl
-  );
-  useAttachAttribute(
-    onboarding,
-    'full-terms-of-service-url',
-    fullTermsOfServiceUrl
-  );
-
-  useAttachAttribute(onboarding, 'privacy-policy-url', privacyPolicyUrl);
-
-  useAttachAttribute(
-    onboarding,
-    'skip-terms-of-service-collection',
-    skipTermsOfServiceCollection
-  );
-
-  useAttachEvent(onboarding, ConnectElementEventNames.exit, onExit);
+    privacyPolicyUrl,
+    recipientTermsOfServiceUrl,
+    skipTermsOfServiceCollection,
+  ]);
 
   return wrapper;
 };
