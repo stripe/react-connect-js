01f934c03541e4ff7372e29af72aad9eb4f56861
Michael Hines <michaelphines@stripe.com>
2025-05-27 18:38:15 -0500

    Update src/useCreateComponent.tsx
    Signed-off-by: Michael Hines <michaelphines@stripe.com>

--- src/useCreateComponent.tsx
+++ src/useCreateComponent.tsx
@@ -16,7 +16,7 @@ export const useCreateComponent = <T extends ConnectElementTagName>(
   const wrapperDivRef = React.useRef<HTMLDivElement | null>(null);
 
   // We would prefer to use display: contents, but it's not well-supported
-const wrapper = <div style={{width: '100%'}} data-stripe-connect-js-wrapper ref={wrapperDivRef}></div>;
+  const wrapper = <div style={{width: '100%'}} data-stripe-connect-js-wrapper ref={wrapperDivRef}></div>;
 
   React.useLayoutEffect(() => {
     if (wrapperDivRef.current !== null && component === null) {
