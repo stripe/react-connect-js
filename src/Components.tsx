830bf90f08b2223ffab99e2a121a265a18c0aca2
H'Liana <102633022+hliana-stripe@users.noreply.github.com>
2025-07-30 15:35:59 -0700

    Add IP Promo component to preview (#164)
    * Add IP Promo component to preview
    
    * lint fix

--- src/Components.tsx
+++ src/Components.tsx
@@ -912,3 +912,44 @@ export const ConnectReportingChart = ({
 
   return wrapper;
 };
+
+export const ConnectInstantPayoutsPromotion = ({
+  onInstantPayoutsPromotionLoaded,
+  onInstantPayoutCreated,
+  onLoadError,
+  onLoaderStart,
+}: {
+  onInstantPayoutsPromotionLoaded?: ({
+    promotionShown,
+  }: {
+    promotionShown: boolean;
+  }) => void;
+  onInstantPayoutCreated?: ({payoutId}: {payoutId: string}) => void;
+} & CommonComponentProps): JSX.Element => {
+  const {wrapper, component: instantPayoutsPromotion} = useCreateComponent(
+    'instant-payouts-promotion'
+  );
+
+  useUpdateWithSetter(instantPayoutsPromotion, onLoaderStart, (comp, val) => {
+    comp.setOnLoaderStart(val);
+  });
+  useUpdateWithSetter(instantPayoutsPromotion, onLoadError, (comp, val) => {
+    comp.setOnLoadError(val);
+  });
+  useUpdateWithSetter(
+    instantPayoutsPromotion,
+    onInstantPayoutsPromotionLoaded,
+    (comp, val) => {
+      comp.setOnInstantPayoutsPromotionLoaded(val);
+    }
+  );
+  useUpdateWithSetter(
+    instantPayoutsPromotion,
+    onInstantPayoutCreated,
+    (comp, val) => {
+      comp.setOnInstantPayoutCreated(val);
+    }
+  );
+
+  return wrapper;
+};
